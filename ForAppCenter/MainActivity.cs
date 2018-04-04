38fad757db66bd493cc8a115533828c09858cff7
Tõnis Harma <tonis.harma@tptlive.ee>
2018-04-04 08:29:26 +0300

    Initial Commit

---
+++ ForAppCenter/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ForAppCenter
+{
+    [Activity(Label = "ForAppCenter", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            AppCenter.Start("ca3cb7c0-5b86-479e-a2ce-ff32fa2029c2",
+                   typeof(Analytics), typeof(Crashes));
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
